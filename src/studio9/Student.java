b4f7c8bad6b5f2c9971e916ef7742e52c34c00b4
Dennis Cosgrove <cosgroved@wustl.edu>
2021-04-21 00:14:32 -0500

    Spring 2021 Initial Commit

---
+++ src/studio9/Student.java
@@ -0,0 +1,24 @@
+package studio9;
+
+//TODO: Copy your Student class from Assignment 7 into this file.
+public class Student {
+	public Student(String firstName, String lastName, int id) {
+		throw new RuntimeException("TODO: Copy your Student class from Assignment 7 into this file.");
+	}
+
+	public String getFullName() {
+		throw new RuntimeException("TODO: Copy your Student class from Assignment 7 into this file.");
+	}
+
+	public int getId() {
+		throw new RuntimeException("TODO: Copy your Student class from Assignment 7 into this file.");
+	}
+
+	public void depositBearBucks(double amount) {
+		throw new RuntimeException("TODO: Copy your Student class from Assignment 7 into this file.");
+	}
+
+	public double getBearBucksBalance() {
+		throw new RuntimeException("TODO: Copy your Student class from Assignment 7 into this file.");
+	}
+}
---
+++ src/studio9/UniversityDatabase.java
@@ -0,0 +1,24 @@
+package studio9;
+
+public class UniversityDatabase {
+	//TODO: Complete this class according to the studio instructions
+
+	public void addStudent(String accountName, Student student) {
+		// TODO
+	}
+
+	public int getStudentCount() {
+		// TODO
+		return 0;
+	}
+
+	public String lookupFullName(String accountName) {
+		// TODO: Complete according to studio instructions
+		return null;
+	}
+
+	public double getTotalBearBucks() {
+		// TODO
+		return 0.0;
+	}
+}
