c54c0bdede8cc76ec3ae983cb16e5da3479374a5
Doug Shook <dshook@wustl.edu>
2023-11-26 05:31:24 -0600

    Removing UniversityDatabase, adding Polynomial

---
+++ src/studio9/Polynomial.java
@@ -0,0 +1,97 @@
+package studio9;
+
+import java.util.LinkedList;
+
+public class Polynomial {
+	
+	private LinkedList<Double> list;
+
+	/**
+	 * Constructs a Polynomial with no terms yet.
+	 */
+	public Polynomial() {
+		//FIXME
+	}
+
+	/*
+	 * Returns a String of the polynomial with the proper form:
+	 * 
+	 * Cx^N + Cx^N-1 + ... + Cx + C
+	 */
+	public String toString() {
+		return ""; //FIXME
+	}
+	/**
+	 * 
+	 * @param coeff
+	 * @return polynomial with added term
+	 */
+	public void addTerm(double coeff) {
+		//FIXME
+	}
+	/**
+	 * 
+	 * @param x
+	 * @return value of polynomial at that x
+	 */
+	public double evaluate(double x) {
+		return 0;//FIXME
+	}
+
+	
+	public Polynomial derivative() {
+		return null;//FIXME
+	}
+	
+
+	/**
+	 * This is the "equals" method that is called by
+	 *    assertEquals(...) from your JUnit test code.
+	 *    It must be prepared to compare this Polynomial
+	 *    with any other kind of Object (obj).  Eclipse
+	 *    automatically generated the code for this method,
+	 *    after I told it to use the contained list as the basis
+	 *    of equality testing.  I have annotated the code to show
+	 *    what is going on.
+	 */
+
+	public boolean equals(Object obj) {
+		// If the two objects are exactly the same object,
+		//    we are required to return true.  The == operator
+		//    tests whether they are exactly the same object.
+		if (this == obj)
+			return true;
+		// "this" object cannot be null (or we would have
+		//    experienced a null-pointer exception by now), but
+		//    obj can be null.  We are required to say the two
+		//    objects are not the same if obj is null.
+		if (obj == null)
+			return false;
+
+		//  The two objects must be Polynomials (or better) to
+		//     allow meaningful comparison.
+		if (!(obj instanceof Polynomial))
+			return false;
+
+		// View the obj reference now as the Polynomial we know
+		//   it to be.  This works even if obj is a BetterPolynomial.
+		Polynomial other = (Polynomial) obj;
+
+		//
+		// If we get here, we have two Polynomial objects,
+		//   this and other,
+		//   and neither is null.  Eclipse generated code
+		//   to make sure that the Polynomial's list references
+		//   are non-null, but we can prove they are not null
+		//   because the constructor sets them to some object.
+		//   I cleaned up that code to make this read better.
+
+
+		// A LinkedList object is programmed to compare itself
+		//   against any other LinkedList object by checking
+		//   that the elements in each list agree.
+
+		return this.list.equals(other.list);
+	}
+
+}
---
+++ src/studio9/PolynomialTest.java
@@ -0,0 +1,121 @@
+package studio9;
+
+import java.util.Random;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class PolynomialTest {
+	private Polynomial p0 = new Polynomial();
+	private Polynomial p1 = synthPoly(new double[] {  4, -7,  0, 11});
+	private Polynomial p2 = synthPoly(new double[] {  7,  5, -2,  5});
+	private Polynomial p3 = synthPoly(new double[] { -1,  2,  3, -4, 5});
+	private Random random = new Random();
+
+	/**
+	 * Helper method to generate a Polynomial from an array of doubles.
+	 * @param coeffs
+	 * @return
+	 */
+	private Polynomial synthPoly(double[] coeffs) {
+		Polynomial ans = new Polynomial();
+		for (double d : coeffs) {
+			ans.addTerm(d);
+		}
+		return ans;
+	}
+
+	
+	@Test
+	public void testDeriv() {
+		Polynomial p1Deriv = synthPoly(new double[] { -7,  0, 33});
+		Polynomial p2Deriv = synthPoly(new double[] {  5, -4, 15 });
+
+		assertEquals(p1Deriv, p1.derivative());
+		assertEquals(p2Deriv, p2.derivative());
+		assertEquals(p0, p0.derivative());
+	}
+
+	@Test
+	public void testEval() {
+		assertEquals(49.0, p2.evaluate(2.0), .01);
+		assertEquals(0.0,  p0.evaluate(100), .01);
+	}
+
+	/**
+	 * Tests the derivative method of the polynomial class using randomly generated inputs
+	 */
+	@Test
+	public void testDerivRandom() {
+		
+		for(int i = 0; i < 100; i ++)
+		{
+			//get a polynomial with random length between 1 and 8
+			double [] coeffs = genRandomCoeffs(random.nextInt(7)+1);
+			Polynomial p1 = synthPoly(coeffs);
+			Polynomial der = new Polynomial();
+			
+			for (int j=0; j < coeffs.length; ++j) {
+				if (j > 0) der.addTerm(coeffs[j]*j);
+			}
+			if(der.toString().compareTo(p1.derivative().toString()) != 0)
+			{
+				printError("Error computing derivative of: " + p1.toString() + ": Use the power rule on each term of the polynomial ((exp * coefficient)x^(exp - 1)). Make sure that your order of operations is correct.", der, p1.derivative());
+			}
+			assertEquals(der, p1.derivative());
+		}
+	}
+
+	/**
+	 * Use randomly generated input to test the eval method of the Polynomial class
+	 */
+	@Test
+	public void testEvalRandom() {
+		
+		for(int i = 0; i < 100; i++) {
+			double [] coeffs = genRandomCoeffs(random.nextInt(7)+1);
+			Polynomial p1 = synthPoly(coeffs);
+			double result = 0;
+			double x = random.nextDouble() * 100;
+			
+			for(int j = 0; j < coeffs.length; j++)
+			{
+				result = result + coeffs[j] * Math.pow(x, (double)j);
+			}
+			
+			if(p1.evaluate(x) > result + 1 || p1.evaluate(x) < result - 1)
+			{
+				System.out.println("Error evaluating: " + p1.toString() + "This recursive method should be run once for each term in the polynomial. Try passing an iterator to your helper method to track your progress. How do you know when the computation is complete?\nExpected: " + result + "\nActual: " + p1.evaluate(x));
+			}
+			assertEquals(result, p1.evaluate(x), 1);
+		}
+	}
+
+	/**
+	 * Geneates an array of random coefficients
+	 * @param num The size of the array
+	 * @return an array of random numbers, each in the range 0...99
+	 */
+	private double[] genRandomCoeffs(int num) {
+		double[] ans = new double[num];
+		for (int i=0; i < num; ++i) {
+			ans[i] = random.nextInt(100);
+		}
+		return ans;
+	}
+
+	
+	@Test
+	public void testPrint() {
+		System.out.println("p2 is " +p2);
+		System.out.println("p2's derivative is " + p2.derivative());
+		System.out.println("new poly is " + new Polynomial());
+	}
+	
+	private void printError(String msg, Polynomial expected, Polynomial actual)
+	{
+		System.out.println(msg);
+		System.out.println("Expected: " + expected.toString());
+		System.out.println("Actual: " + actual.toString());
+	}
+}
